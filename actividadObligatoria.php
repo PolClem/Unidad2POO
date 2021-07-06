062f8b40c64ad76db9bed583246d354b1666572a
María Paula Clemente <77366159+PolClem@users.noreply.github.com>
2021-07-05 23:20:55 -0300

    Add files via upload

---
+++ actividadObligatoria.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Avtividad Obligatoria PHP Unidad 2</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="actividadObligatoria.php">🏠</a> 
+        </nav>
+    </header>
+
+    <main>
+        <section>
+            <div>
+                <?php
+                  echo "<h2>";
+                  echo "María Paula Clemente";
+                  echo "<br>";
+                  echo "36 años";
+                  echo "<br>";
+                  echo "mis hobbies son: jugar a los jueguitos, andar en bicicleta, amo los cactus, y por supuesto se mamá";
+                  echo "<br>";
+                  echo "Mi editor preferido es el Visual Studio Code";
+                  echo "<br>";
+                  echo "El sistema operativo que utilizo es Windows 10."
+                ?>
+            </div>  
+        </section>
+    </main>
+
+    <footer>
+        <h5>Todos los derechos reservados ©PolClem</h5>
+    </footer>
+</body>
+</html>
